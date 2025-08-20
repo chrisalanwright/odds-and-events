7cb3ad640e8813fb15ad943ef607460c9a13985e
Christopher Wright <chrisalanwright@gmail.com>
2025-08-20 16:05:49 -0400

    added-sort

--- index.js
+++ index.js
@@ -6,3 +6,16 @@ function addToBank(number) {
   bank.push(number);
   render();
 }
+
+function sortBank() {
+  odds = bank.filter((num) => num % 2 !== 0);
+  evens = bank.filter((num) => num % 2 === 0);
+  render();
+}
+
+function sort() {
+  if (bank.length === 0) {
+    alert("Please add some numbers to the bank first.");
+    return;
+  }
+}
